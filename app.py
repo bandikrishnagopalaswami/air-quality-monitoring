45f96ace9f8db0ba3b871707a61b7d6f952ba0bc
Bandi Krishna Gopala Swami <152390353+bandikrishnagopalaswami@users.noreply.github.com>
2025-11-26 07:55:11 +0530

    trigger pipeline

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+from flask import Flask, render_template, request
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 app = Flask(__name__)
 
